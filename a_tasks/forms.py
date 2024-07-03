e8df3536ec5430cba983513999c3e2da44fbac62
Jonatan Snyders <jonatansnyders@gmail.com>
2024-07-03 18:33:27 +0200

    task edit - added due_date and review_date

--- a_tasks/forms.py
+++ a_tasks/forms.py
@@ -4,7 +4,7 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description']
+        fields = ['title', 'description', 'due_date', 'review_date']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'bg-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 rounded px-2 py-1'}),
             'description': forms.Textarea(attrs={'class': 'bg-gray-800 text-gray-200 w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-purple-600', 'rows': 4}),
