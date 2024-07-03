954cf064c23b69e3d90255f08720b162a91321a5
Jonatan Snyders <jonatansnyders@gmail.com>
2024-07-03 18:45:56 +0200

    task edit - added type (required a bit of alpine.js)

--- a_tasks/forms.py
+++ a_tasks/forms.py
@@ -5,7 +5,7 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'due_date', 'review_date', 'board', 
-                  'review_stage', 'priority']
+                  'review_stage', 'priority', 'type']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'bg-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 rounded px-2 py-1'}),
             'description': forms.Textarea(attrs={'class': 'bg-gray-800 text-gray-200 w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-purple-600', 'rows': 4}),
