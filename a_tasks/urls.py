967339fdb89d792a89c85db4c03f12892e33479e
Jonatan Snyders <jonatansnyders@gmail.com>
2024-07-03 15:02:52 +0200

    consolidated view_task and view_update_task URLs to both point to the view_update_task view

--- a_tasks/urls.py
+++ a_tasks/urls.py
@@ -22,7 +22,7 @@ app_name = 'tasks'
 urlpatterns = [
     path('', views.list_view, name='home'),
     # path('task/create/', views.create_task, name='create_task'),
-    path('task/<str:id>/', views.view_task, name='view_task'),
+    path('task/<str:id>/', views.view_update_task, name='view_task'),
     path('task/<str:id>/edit/', views.view_update_task, name='view_update_task'),
     path('task/<str:id>/complete/', views.toggle_task_completed, name='toggle_task_completed'),
 ]
