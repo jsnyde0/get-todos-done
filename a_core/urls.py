c136daeafd42ee1f747b3f3cf836b4b9664cd210
Jonatan Snyders <jonatansnyders@gmail.com>
2024-07-09 13:03:26 +0200

    fixed honeypot url

--- a_core/urls.py
+++ a_core/urls.py
@@ -18,9 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('thebaws/', admin.site.urls),
     path('', include('a_tasks.urls')),
     path('accounts/', include('allauth.urls')),
-    path('thebaws/', admin.site.urls),
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
 ]
