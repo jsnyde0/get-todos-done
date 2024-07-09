289ae2a842d3e94dc8e21e0ccf621f3452c54f31
Jonatan Snyders <jonatansnyders@gmail.com>
2024-07-09 18:05:09 +0200

    set up mounted disk to persist SQLite

--- a_core/settings.py
+++ a_core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from environ import Env
 
@@ -107,10 +108,15 @@ WSGI_APPLICATION = 'a_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+if DEBUG:
+    DB_NAME = BASE_DIR / 'db.sqlite3'
+else:
+    DB_NAME = os.path.join('/data', 'db.sqlite3')  # the mounted disk path
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DB_NAME,
     }
 }
 
